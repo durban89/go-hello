70e98db560c0fab68028b8effda189a138ae4a3f
durban.zhang <durban.zhang@gmail.com>
2017-04-18 14:39:22 +0800

    update

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main(){
+	"github.com/durban.zhang/stringutil"
+)
+
+func main() {
 	fmt.Printf("Hello, world. \n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
